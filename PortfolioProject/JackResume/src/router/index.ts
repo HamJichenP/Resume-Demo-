32c00d7cbcd995caebdf3ada3f980842a635349e
HamJichenP <chenperfect0328@gmail.com>
2025-11-20 16:51:55 +0800

    Homeview 改名 ResumeView

--- PortfolioProject/JackResume/src/router/index.ts
+++ PortfolioProject/JackResume/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/HomeView.vue'
+import Home from '../views/ResumeView.vue'
 import About from '../views/AboutView.vue'
 
 const routes = [
